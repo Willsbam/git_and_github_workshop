0f162c482a101c0c1f786568294fb6190769b171
Willsbam <wlbramley@gmail.com>
2025-05-05 11:32:53 -0400

    dog functoionality

--- animal.py
+++ animal.py
@@ -2,8 +2,14 @@ import sys
 def default():
     print("Hellow WOrld!")
 
+def dog():
+    print("Woof")
+
 def main():
-    default()
+    if sys.argv[1]=="dog"L
+        dog()
+    else:
+        default()
 
 if __name__=='__main__":
     main():
